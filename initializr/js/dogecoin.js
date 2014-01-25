085522fae9cc7e14e4ab27112f53feed44704071
MaxWofford <max.speed.wofford@gmail.com>
2014-01-25 14:38:29 -0800

    Added adblock meta tag

--- initializr/js/dogecoin.js
+++ initializr/js/dogecoin.js
@@ -1,3 +1,5 @@
+//dogecoin.js from dogecoin.com/dogecoin.js 
+
 $(document).ready(function() {
     /*$.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fcoinmarketcap.com%2F'%20and%20xpath%3D'%2F%2Fa%5Bcontains(%40href%2C%22http%3A%2F%2Fwww.cryptocoincharts.info%23jump-doge-btc%22)%5D'&format=json", function(data) {
         price = (data.query.results.a).replace(/ /g, '');
